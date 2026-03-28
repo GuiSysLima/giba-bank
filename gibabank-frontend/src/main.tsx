2aa7f74598edaf630695361776de25189781f38c
Guilherme Felix <guilhermerutemfelix@gmail.com>
2026-03-28 13:50:36 -0300

    feat(frontend): axios config

--- gibabank-frontend/src/main.tsx
+++ gibabank-frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { AuthProvider } from './contexts/AuthContext.tsx'
 import './index.css'
-import App from './App.tsx'
+import { Login } from './pages/Login/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <AuthProvider>
+    <Login></Login>
+  </AuthProvider>,
 )
